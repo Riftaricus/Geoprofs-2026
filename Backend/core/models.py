1468e02aea5b1d7676cb202136a424d4e60d117d
M3v_ <max@m3v.dev>
2026-09-16 14:49:56 +0200

    feat(model.py): added userdata model

--- Backend/core/models.py
+++ Backend/core/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 class Audit_Log(models.Model):
     comment = models.CharField(max_length=100)
-    timestamp = models.DateTimeField()
-    log_type = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    logType = models.CharField(max_length=100)
 
-    #user = models.ForeignKey()
+class UserData(models.Model):
+    # id = models.ForeignKey(to='Account.id', on_delete=models.CASCADE)
+    fullName = models.CharField(max_length=100)
+    phoneNumber = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
