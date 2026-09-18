de4f611cadbd47d5beb7cd839ff91e29063d30cc
Rift <61116182+Riftaricus@users.noreply.github.com>
2026-09-18 12:36:18 +0200

    Feat(models.py): Added the leave model to the models in the backend

--- Backend/core/models.py
+++ Backend/core/models.py
@@ -6,3 +6,23 @@ class Audit_Log(models.Model):
     log_type = models.CharField(max_length=100)
 
     #user = models.ForeignKey()
+
+class Leave(models.Model):
+    class LeaveStatus(models.TextChoices):
+        PENDING = "pending"
+        ACCEPTED = "accepted"
+        DENIED = "denied"
+
+    #user = models.ForeignKey()
+
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    reason = models.CharField(max_length=100)
+
+    status = models.CharField(
+        choices=LeaveStatus.choices,
+        default=LeaveStatus.PENDING
+    )
+
+    comment = models.CharField(max_length=100, null=True, blank=True)
