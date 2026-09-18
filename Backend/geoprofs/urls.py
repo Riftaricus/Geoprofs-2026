fbe4bf75d1ba2e883fbf58d2806cce681418ebf5
Rift <61116182+Riftaricus@users.noreply.github.com>
2026-09-18 13:50:39 +0200

    Transfered everything from the old backend into the new backend

--- Backend/geoprofs/urls.py
+++ Backend/geoprofs/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
+from django.urls import path
 from rest_framework import routers
 from django.urls import include, path
-from rest_framework.urlpatterns import format_suffix_patterns
 
-from core import views
+import api_geoprofs.views as views
 
 router = routers.DefaultRouter()
 
